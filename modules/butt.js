c5ef9613346633353943941de1b487bf69bcffd0
Edwin Pers <pers.edwin@gmail.com>
2014-12-10 02:19:41 -0500

    Update butt.js
    ignore articles

--- modules/butt.js
+++ modules/butt.js
@@ -25,7 +25,11 @@ var butt =
                 var caps = new Buffer(crypto.randomBytes(1)).readUInt8(0);
                 var choice = new Buffer(crypto.randomBytes(1)).readUInt8(0) % butt.stuff.length;
                 choice = butt.stuff[choice];
-
+                while (choice == "a" || choice == "the" || choice == "an")
+                {
+                    var choice = new Buffer(crypto.randomBytes(1)).readUInt8(0) % butt.stuff.length;
+                    choice = butt.stuff[choice];
+                }
                 if(plural % 2)
                     choice += "s";
 
@@ -53,7 +57,7 @@ var butt =
         if(chance > 249)
         {
             message = butt.generate(message);
-
+            
             // If this is a channel message
             if(to.charAt(0) == '#')
             {
