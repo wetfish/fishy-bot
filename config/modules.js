b1718a5bb32f8d7eef71c3ffd454a767a6258602
Rachel Fish <itsrachelfish@gmail.com>
2016-10-14 04:20:11 -0600

    Small config changes

--- config/modules.js
+++ config/modules.js
@@ -1,1 +1,1 @@
-module.exports = ['auth', 'admin', 'butt', 'github', 'slapanus', 'topic', 'poisonshits', 'ross', 'triplicates', 'principles', 'webhook'];
+module.exports = ['auth', 'admin', 'butt', 'github', 'slapanus', 'topic', 'poisonshits', 'ross', 'triplicates', 'principles', 'webhook', 'rainbow'];
