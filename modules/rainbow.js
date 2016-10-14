bb9f1cfdc941a27bb11d38e8f9aeb36281b81662
Rachel Fish <itsrachelfish@gmail.com>
2016-10-14 04:18:55 -0600

    Added hostname based timeouts for rainbow commands

--- modules/rainbow.js
+++ modules/rainbow.js
@@ -8,6 +8,7 @@ var rainbow =
     client: false,
     methods: ['message'],
     commands: ['rainbow', 'bigrainbow', 'biggerrainbow', 'metalrainbow'],
+    timeout: {},
 
     // This really should be a core function or something
     reply: function(type, from, to, message)
@@ -44,6 +45,26 @@ var rainbow =
         }
     },
 
+    checkTimeout: function(host)
+    {
+        if(rainbow.timeout[host])
+        {
+            var timeout = (rainbow.timeout[host].getTime() - new Date().getTime()) / 1000;
+            return timeout;
+        }
+    },
+
+    setTimeout: function(host, duration)
+    {
+        var date = new Date();
+        rainbow.timeout[host] = new Date(date.getTime() + (duration * 1000));
+
+        setTimeout(function()
+        {
+            rainbow.timeout[host] = false;
+        }, duration * 1000);
+    },
+
     toilet: function(from, to, options, message)
     {
         var toilet = spawn('toilet', options);
@@ -58,40 +79,50 @@ var rainbow =
 
     rainbow: function(from, to, message, details)
     {
+        if(rainbow.checkTimeout(details.host))
+        {
+            rainbow.reply('say', from, to, 'nah bro');
+            return;
+        }
+
         rainbow.toilet(from, to, ['--gay', '--irc', '--font', 'term'], message);
+        rainbow.setTimeout(details.host, 20);
     },
 
     bigrainbow: function(from, to, message, details)
     {
-        if(message.length > 256)
+        if(message.length > 256 || rainbow.checkTimeout(details.host))
         {
             rainbow.reply('say', from, to, 'nah bro');
             return;
         }
 
         rainbow.toilet(from, to, ['--gay', '--irc', '--font', 'future'], message);
+        rainbow.setTimeout(details.host, 60);
     },
 
     biggerrainbow: function(from, to, message, details)
     {
-        if(message.length > 128)
+        if(message.length > 128 || rainbow.checkTimeout(details.host))
         {
             rainbow.reply('say', from, to, 'nah bro');
             return;
         }
 
         rainbow.toilet(from, to, ['--gay', '--irc', '--font', 'mono9'], message);
+        rainbow.setTimeout(details.host, 120);
     },
 
     metalrainbow: function(from, to, message, details)
     {
-        if(message.length > 128)
+        if(message.length > 128 || rainbow.checkTimeout(details.host))
         {
             rainbow.reply('say', from, to, 'nah bro');
             return;
         }
 
         rainbow.toilet(from, to, ['--metal', '--irc', '--font', 'mono9'], message);
+        rainbow.setTimeout(details.host, 120);
     },
 
     bind: function()
