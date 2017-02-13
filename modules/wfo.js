fa638ac0e5aa64e03f8704813696a91037faba9d
nicefish <nicefish@wetfishonline.forums>
2017-02-12 19:27:58 -0500

    Better error handling, better flood protection

--- modules/wfo.js
+++ modules/wfo.js
@@ -6,55 +6,21 @@ var FeedParser = require('feedparser');
 var request = require('request');
 
 ////////////////////////////////////////////////////////////////////////////////
-function MessageCounter(window_length) {
-    this.window_length = window_length;
-    this.message_times = [];
-}
-// Introduce a message and also return the number of messages in the
-// window period.
-MessageCounter.prototype.boop = function () {
-    var curtime = Date.now();
-    this.message_times.push(curtime);
-    var thismessagecounter = this;
-    this.message_times = this.message_times.filter(function (x) {
-        return x >= curtime - thismessagecounter.window_length;
-    });
-    return this.message_times.length;
-};
-MessageCounter.prototype.reset = function () {
-    this.message_times = [];
-}
-
-////////////////////////////////////////////////////////////////////////////////
 function State() {
     this.STATE_NOTREADY = 0; // Before we have seen_guids, so we don't know what msgs are new.
     this.STATE_NORMAL = 1;   // We're active.
-    this.STATE_BACKOFF = 2;  // We're chilling out for a bit because we just came.
 
-    // If we try to send more than TRIGGER_NUM_MESSAGES in
-    // TRIGGER_INTERVAL unit of time, let's chill out for
-    // BACKOFF_TIMEOUT seconds until checking again.
-    //
-    // If you don't want to miss messages silently because they
-    // overflow the feed, you should set it up so that
-    // TRIGGER_NUM_MESSAGES is less than the number of items the feed
-    // returns.
-    this.TRIGGER_NUM_MESSAGES = 5;
-    this.TRIGGER_INTERVAL = 10 * (60 * 1000 /*minutes*/);
-    this.NORMAL_TIMEOUT = 2 * (60 * 1000);
-    this.BACKOFF_TIMEOUT = 30 * (60 * 1000);
+    this.TIMEOUT = 5 * (60 * 1000);
+    this.LIMIT = 2; // Max num items we would write about each time?
 
     this.seen_guids = []; // TODO: Clean up old items.
     this.current_state = this.STATE_NOTREADY;
-    this.msgcounter = new MessageCounter(this.TRIGGER_INTERVAL);
 }
 State.prototype.getCurrentTimeout = function() {
     if (this.current_state == this.STATE_NOTREADY) {
-        return 0;
+        return 10000; // This is used when there's an error before we're ready.
     } else if (this.current_state == this.STATE_NORMAL) {
-        return this.NORMAL_TIMEOUT;
-    } else if (this.current_state == this.STATE_BACKOFF) {
-        return this.BACKOFF_TIMEOUT;
+        return this.TIMEOUT;
     }
     console.log('getCurrentTimeout error');
 }
@@ -78,33 +44,25 @@ State.prototype.newItems = function(items, announce_fn) {
         // announce oldest first.
         need_announce.reverse();
 
-        for (var i in need_announce) {
-            var item = need_announce[i];
-            announce_fn(util.format("9[WFO] %s 9posted %s 9at %s",
-                                    item.author,
-                                    item.title,
-                                    item.guid));
+        if (need_announce.length >= this.LIMIT) {
 
-            if (this.msgcounter.boop() > this.TRIGGER_NUM_MESSAGES) {
-                announce_fn("9[WFO] 4Too much shit's going on. I'll chill the fuck out for a bit. Join the action at 9https://wetfishonline.com/ 4!");
+            announce_fn(util.format("9[WFO] At least 9%d posts were made in the last %d minutes! Join the action at 9https://wetfishonline.com/",
+                                    need_announce.length,
+                                    this.TIMEOUT / 60 / 1000));
 
-                console.log('WFO entering STATE_BACKOFF');
-                this.current_state = this.STATE_BACKOFF;
-                this.seen_guids = [];
-                this.msgcounter.reset();
-                return;
+        } else if (need_announce.length > 0) {
+            for (var i in need_announce) {
+                var item = need_announce[i];
+                announce_fn(util.format("9[WFO] %s 9posted %s 9at %s",
+                                        item.author,
+                                        item.title,
+                                        item.guid));
             }
         }
     }
 
-    // We populated seen_guilds, so now we're ready.
+    // We populated seen_guids, so now we're ready.
     if (this.current_state == this.STATE_NOTREADY) {
-        console.log('WFO entering STATE_NORMAL');
-        this.current_state = this.STATE_NORMAL;
-    }
-
-    // We just got the item list after backoff. We're ready now!
-    if (this.current_state == this.STATE_BACKOFF) {
         console.log('WFO entering STATE_NORMAL');
         this.current_state = this.STATE_NORMAL;
     }
@@ -127,8 +85,7 @@ var wfo =
 
         var feedparser = new FeedParser();
         feedparser.on('error', function (error) {
-            console.log('feedparser error');
-            console.log(error);
+            console.log(new Date(), 'WFO feedparser error.', error);
         });
         feedparser.on('readable', function (error) {
             var stream = this;
@@ -140,16 +97,31 @@ var wfo =
             }
         });
         feedparser.on('finish', function () {
-            wfo.state.newItems(wfo.buffer,
-                                  function (line) {
-                                      wfo.client.say('#wetfish', line);
-                                  });
+            var stream = this;
+
+            // We get here even when there's an error.
+            if (wfo.buffer.length > 0) {
+                wfo.state.newItems(wfo.buffer,
+                                   function (line) {
+                                       wfo.client.say('#botspam', line);
+                                   });
+            }
             wfo.timeout = setTimeout(wfo.tick, wfo.state.getCurrentTimeout());
         });
 
         var req = request(URI, {timeout: 5000});
         req.on('error', function (error) {
-            console.log(error);
+            var stream = this;
+
+            console.log(new Date(), 'WFO request error.', error);
+
+            if (!stream.cancel_for_error) {
+                console.log(new Date(), 'WFO Going to wait for the next tick.');
+                wfo.timeout = setTimeout(wfo.tick, wfo.state.getCurrentTimeout());
+            }
+
+            // If we get an error it may not be the only one.
+            stream.cancel_for_error = true;
         });
         req.on('response', function (res) {
             // If we're asked to stop and we got the http response
