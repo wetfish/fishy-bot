91f8f8d1523ccd40d1ae3571f84eebc6cbc1df0c
Rachel Fish <itsrachelfish@gmail.com>
2024-03-07 08:38:27 +0000

    Updated server and channel config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = {
-    server: 'temp.wetfish.net',
+    server: 'future.wetfish.net',
     port: 6697,
     secure: true,
     name: 'fishy',
     userName: 'fishybot',
     realName: 'Source available via https://github.com/itsrachelfish/fishy-bot',
-    channels: ['#wetfish', '#botspam', '#poomp', '#freefish', '#crypto']
+    channels: ['#wetfish', '#botspam', '#freefish', '#crypto', '#poopfish']
 };
